2754b22965ff625c49404202a04307383cea8236
monsteronfire <monsteronfire@gmail.com>
2016-07-29 10:07:34 +0800

    Update controller actions

--- app/components/friends/edit-form.js
+++ app/components/friends/edit-form.js
@@ -4,9 +4,11 @@ export default Ember.Component.extend({
   actions: {
     save() {
       console.log('+- save action in edit-form component');
+      this.save(this.get('model'));
     },
     cancel() {
       console.log('+- cancel action in edit-form component');
+      this.cancel();
     }
   }
 });
--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    save(model) {
+      console.log('+--- save action called in friends new controller');
+    },
+
+    cancel() {
+      console.log('+--- cancel action called in friends new controller');
+    }
+  }
 });
