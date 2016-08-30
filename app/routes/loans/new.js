7e4b0c4c18af596577d4056be66618443328ea2d
monsteronfire <monsteronfire@gmail.com>
2016-08-30 08:27:50 +0800

    Add loan edit-form

--- app/routes/loans/new.js
+++ app/routes/loans/new.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.createRecord('loan', {
-      friend: this.modelFor('friends/show');
+      friend: this.modelFor('friends/show')
     });
   }
 });
