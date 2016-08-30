d6cb4216fe87898b23b70feef35b711a25d1f892
monsteronfire <monsteronfire@gmail.com>
2016-08-30 09:21:48 +0800

    Add save and cancel actions to loans edit-form component
    * Have yet to start "Computed Property Macros"

---
+++ app/components/loans/edit-form.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  articles: Ember.computed({
+    get() {
+      return this.get('store').findAll('article');
+    }
+  }).readOnly(),
+
+  save() {
+    this.get('model').save().then( (model) => {
+      this.back(model.get('friend'));
+    }, () => {
+      this.set(
+        'errorMessage',
+        'there was something wrong saving the loan'
+      );
+    });
+  },
+
+  cancel() {
+    this.back(this.get('model.friend'));
+  }
+});
