71df77a52ad5b2e9cd0219bc67b57088a6eb88b7
monsteronfire <monsteronfire@gmail.com>
2016-09-02 08:24:59 +0800

    Impement resetController on routes * Clears unsaved changes that have not been persisted to the backend because of a cancellation

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -3,5 +3,24 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.createRecord('friend');
+  },
+
+  activate() {
+    console.log('----- activate hook called -----');
+  },
+
+  deactivate() {
+    console.log('----- deactivate hook called -----');
+  },
+
+  resetController: function (controller, isExiting, transition) {
+    if(isExiting) {
+      console.log('----- resetController hook called -----');
+      var model = controller.get('model');
+
+      if (model.get('isNew') ) {
+        model.destroyRecord();
+      }
+    }
   }
 });
--- app/routes/loans/new.js
+++ app/routes/loans/new.js
@@ -5,5 +5,12 @@ export default Ember.Route.extend({
     return this.store.createRecord('loan', {
       friend: this.modelFor('friends/show')
     });
+  },
+
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      var model = controller.get('model');
+      model.rollback();
+    }
   }
 });
