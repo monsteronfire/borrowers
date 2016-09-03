c1b8fece941f82e12619ef01aba2681ed2a3875d
monsteronfire <monsteronfire@gmail.com>
2016-09-04 00:10:00 +0800

    Add init to loan-row component

--- app/components/loans/loan-row.js
+++ app/components/loans/loan-row.js
@@ -2,7 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'tr',
-  loan: null //passed-in
+  loan: null, //passed-in
+
+  init() {
+    this._super(...arguments);
+    this.addObserver('loan.returned', this, this.stateChanged);
+  },
 
   stateChanged: Ember.observer('loan.returned', function () {
     var loan = this.get('loan');
