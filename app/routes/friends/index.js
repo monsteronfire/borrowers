6d7c6d50dcf98c3686597cae74a363bd085c3ca5
monsteronfire <monsteronfire@gmail.com>
2016-09-07 08:17:02 +0800

    Refresh model when query params change

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -1,8 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    //return this.store.query('friend', {include: 'loans, loans.article'});
-    return this.store.query('friend', {include: 'loans'});
+  queryParams: {
+    sortBy: {
+      refreshModel: true
+    },
+
+    sortAscending: {
+      refreshModel: true
+    }
+  },
+
+  model(params) {
+    //let query = { include: 'loans, loans.article' };
+    let query = { include: 'loans' };
+
+    if (params.sortBy) {
+      query.sort = params.sortBy;
+
+      if (!params.sortAscending) {
+        query.sort = `-${query.sort}`;
+      }
+    }
+
+    return this.store.query('friend', query);
   }
 });
